65a13c9b981269226f2a3cfd9be2a7996dfd0246
Mike Kelly <pioto@pioto.org>
2020-12-14 21:10:55 -0500

    enrich our data model a bit, start to think about modeling colors

--- buzybee.py
+++ buzybee.py
@@ -2,13 +2,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+class Status:
+    def __init__(self, code, color):
+        self.code = code
+        self.color = color
+
+
 VALID_STATUS = {
-    'unknown',
-    'available',
-    'busy',
-    'away'
+    'unknown': Status('unknown', b'\x00\x00'),
+    'available': Status('available', b'\x00\x00'),
+    'busy': Status('busy', b'\x00\x00'),
+    'away': Status('away', b'\x00\x00')
 }
-STATUS = 'unknown'
+STATUS = VALID_STATUS['unknown']
 
 
 @app.route('/api/v1/status', methods=['GET', 'POST'])
@@ -21,13 +27,13 @@ def status():
 
 def get_status():
     global STATUS
-    return {"status": STATUS}
+    return {"status": STATUS.code}
 
 
 def update_status():
     global STATUS
     if valid_status(request.form['status']):
-        STATUS = request.form['status']
+        STATUS = VALID_STATUS[request.form['status']]
         return get_status()
     else:
         return {"error": "Invalid Status"}
